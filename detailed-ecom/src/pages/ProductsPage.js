35f9987c4dc069ae4c456646320f7e2c023ee3eb
Anshuman Kundu <kundu4coding@gmail.com>
2023-06-15 15:16:25 +0530

    ProductsPage Done with all functionality

--- detailed-ecom/src/pages/ProductsPage.js
+++ detailed-ecom/src/pages/ProductsPage.js
@@ -1,11 +1,36 @@
 import React from 'react'
-
+import styled from "styled-components";
+import Filters from "../components/Filters";
+import PageHero from "../components/PageHero";
+import Sort from "../components/Sort";
+import ProductList from "../components/ProductList";
 const ProductsPage = () => {
   return (
-    <div>
-      product
-    </div>
+    <main>
+      <PageHero title='products' />
+      <Wrapper className='page'>
+        <div className='section-center products'>
+          <Filters />
+          <div>
+            <Sort />
+            <ProductList />
+          </div>
+        </div>
+      </Wrapper>
+    </main>
   )
 }
 
+const Wrapper = styled.div`
+  .products {
+    display: grid;
+    gap: 3rem 1.5rem;
+    margin: 4rem auto;
+  }
+  @media (min-width: 768px) {
+    .products {
+      grid-template-columns: 200px 1fr;
+    }
+  }
+`
 export default ProductsPage
