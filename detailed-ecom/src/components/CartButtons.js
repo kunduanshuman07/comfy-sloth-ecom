b1157ab6ee1b555cdcef5abc91163796092f738d
Anshuman Kundu <kundu4coding@gmail.com>
2023-06-19 11:10:23 +0530

    Lot of other functionalities added

--- detailed-ecom/src/components/CartButtons.js
+++ detailed-ecom/src/components/CartButtons.js
@@ -1,24 +1,43 @@
-import { FaShoppingCart, FaUserMinus, FaUserPlus } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import { useProductsContext } from "../context/products_context";
-const CartButtons = () => {
-  const { closeSidebar } = useProductsContext();
+import React from 'react'
+import { FaShoppingCart, FaUserMinus, FaUserPlus } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { useProductsContext } from '../context/products_context'
+import { useCartContext } from '../context/cart_context'
+import { useUserContext } from '../context/user_context'
+const CartButton = () => {
+  const { closeSidebar } = useProductsContext()
+  const { total_items, clearCart } = useCartContext()
+  const { loginWithRedirect, myUser, logout } = useUserContext()
   return (
-    <Wrapper className="cart-btn-wrapper">
-      <Link to="/cart" className="cart-btn" onClick={closeSidebar}>
+    <Wrapper className='cart-btn-wrapper'>
+      <Link to='/cart' className='cart-btn' onClick={closeSidebar}>
         Cart
-        <span className="cart-container">
+        <span className='cart-container'>
           <FaShoppingCart />
-          <span className="cart-value">12</span>
+          <span className='cart-value'>{total_items}</span>
         </span>
       </Link>
-      <button type="button" className="auth-btn">
-        Login <FaUserPlus />
-      </button>
+      {myUser ? (
+        <button
+          type='button'
+          className='auth-btn'
+          onClick={() => {
+            clearCart()
+            localStorage.removeItem('user')
+            logout({ returnTo: window.location.origin })
+          }}
+        >
+          Logout <FaUserMinus />
+        </button>
+      ) : (
+        <button type='button' className='auth-btn' onClick={loginWithRedirect}>
+          Login <FaUserPlus />
+        </button>
+      )}
     </Wrapper>
-  );
-};
+  )
+}
 
 const Wrapper = styled.div`
   display: grid;
@@ -72,5 +91,5 @@ const Wrapper = styled.div`
       margin-left: 5px;
     }
   }
-`;
-export default CartButtons;
+`
+export default CartButton
--- detailed-ecom/src/index.js
+++ detailed-ecom/src/index.js
@@ -1,18 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { ProductsProvider } from "./context/products_context";
-import { FilterProvider } from "./context/filter_context";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <App />
-    </FilterProvider>
-  </ProductsProvider>
-);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ProductsProvider } from './context/products_context';
+import { FilterProvider } from './context/filter_context';
+import { CartProvider } from './context/cart_context';
+import { UserProvider } from './context/user_context';
+import { Auth0Provider } from '@auth0/auth0-react';
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(
+  <Auth0Provider
+    domain='dev-qi3j1wxnrsbxtfkk.us.auth0.com'
+    clientId='WsDVPhBYjphPXZQyU54V8m7bGq2CCEJR'
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+    cacheLocation='localstorage'
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
+);
